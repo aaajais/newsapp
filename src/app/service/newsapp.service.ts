09cff111959efcf4d759538f81d27656c5bf117f
aaajais <91136624+aaajais@users.noreply.github.com>
2022-06-28 16:35:32 +0530

    init

--- src/app/service/newsapp.service.ts
+++ src/app/service/newsapp.service.ts
@@ -8,8 +8,8 @@ export class NewsappService {
 
   constructor(private _http:HttpClient) { }
 
-  headlineNews="https://newsapi.org/v2/top-headlines?country=us&apiKey=eebb024f640042d8a6c1a377c29a4adb"
-  
+ 
+  headlineNews="https://newsapi.org/v2/everything?q=bitcoin&apiKey=eebb024f640042d8a6c1a377c29a4adb"
   headline():Observable<any>{
     return this._http.get(this.headlineNews)
   }
